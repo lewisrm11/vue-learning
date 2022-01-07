da4553a82ee514a0794cffec27612cd2e7f0d339
Carlos Levi Ricalde May <levi.ricalde@xid.com.mx>
2022-01-07 03:11:21 -0600

    oficial documentation reading complete

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,67 @@
 <template>
   <div id="app">
+    <button @click="message='test de watch'">Button</button>
+    <br><br>
+    <label style="background-color: black; color:white">{{printMessage}}</label>
+    <br><br>
+    <label style="background-color: red; color:white">{{message}}</label>
+    <br><br>
+    <label style="background-color: blue; color:white">{{watchProp}}</label>
+    <br><br>
+    <label style="background-color: pink; color:white">{{computedWatchProp}}</label>
+    <br><br>
+    <button @click="secuence='computed property / with getter and setter'">Button</button>
+    <br><br>
+    <label style="background-color: gray; color:white">{{secuence}}</label>
+    <br><br>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <br>
+    <br>
+    <br>
+    <input v-model.lazy="message">
   </div>
 </template>
 
 <script>
+//using ES2015 modules, through Babel and Webpack
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  data:function () {
+    return {
+      one: "One",
+      two: "Two",
+      message: "testtttt",
+      watchProp: ""
+    };
+  },
+  computed: {
+    // a computed getter
+    printMessage: function () {
+      // `this` points to the vm instance
+      return "hello vue".split('').reverse().join('')
+    },
+    computedWatchProp: function () {
+      return  this.message + " from button but with a computed prop"
+    },
+    secuence: {
+      get: function () {
+        return this.one + "-" + this.two;
+      },
+      set: function (val) {
+        let names = val.split('/')
+        this.one = names[0]
+        this.two = names[1]
+      }
+    }
+  },
+  watch: {
+    message: function() {
+      this.watchProp = this.message + " from button"
+    },
+  },
+  me: 'App',
   components: {
     HelloWorld
   }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+//Vue.config.productionTip = false
 
 new Vue({
+  created: function () {
+    // `this` points to the vm instance
+    console.log('created stage')
+  },
+  mounted: function () {
+    // `this` points to the vm instance
+    console.log('mounted stage')
+  },
+  updated: function () {
+    // `this` points to the vm instance
+    console.log('updated stage')
+  },
+  destroyed: function () {
+    // `this` points to the vm instance
+    console.log('destroyed stage')
+  },
   render: h => h(App),
 }).$mount('#app')
