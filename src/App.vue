09c0f90665f45fdc27c3d0ff98ba6187bfc1b9bb
Carlos Levi Ricalde May <levi.ricalde@xid.com.mx>
2022-01-08 04:07:10 -0600

    Test form App and bootstrap css

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,86 @@
 <template>
-  <div id="app">
-    <button @click="message='test de watch'">Button</button>
-    <br><br>
-    <label style="background-color: black; color:white">{{printMessage}}</label>
-    <br><br>
-    <label style="background-color: red; color:white">{{message}}</label>
-    <br><br>
-    <label style="background-color: blue; color:white">{{watchProp}}</label>
-    <br><br>
-    <label style="background-color: pink; color:white">{{computedWatchProp}}</label>
-    <br><br>
-    <button @click="secuence='computed property / with getter and setter'">Button</button>
-    <br><br>
-    <label style="background-color: gray; color:white">{{secuence}}</label>
-    <br><br>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <br>
-    <br>
-    <br>
-    <input v-model.lazy="message">
-  </div>
-</template>
+  <main class="container">
+    <div class="my-3 p-3 bg-body rounded shadow-lg">
+      <div class="card">
+        <div class="card-body">
+          <div class="row">
+            <div class="col-10">
+              <form>
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">Item</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                         v-model=descripcion>
+                  <div id="emailHelp" class="form-text">Add item</div>
+                </div>
+                <button type="submit" class="btn btn-primary" @click.prevent="addItem">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
+
+    <div class="my-3 p-3 bg-body rounded shadow-lg">
+      <h6 class="border-bottom pb-2 mb-0">Recent updates</h6>
+      <div class="d-flex text-muted pt-3" v-for="item in items" v-bind:key="item.id">
+        <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
+             xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
+             preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
+          <rect width="100%" height="100%" fill="#007bff"/>
+          <text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text>
+        </svg>
+
+        <p class="pb-3 mb-0 small lh-sm border-bottom">
+          <strong class="d-block text-gray-dark">{{ item.user }}</strong>
+          {{ item.descripcion }}
+        </p>
+      </div>
+    </div>
+
+    <Empty></Empty>
+  </main>
+</template>
 <script>
-//using ES2015 modules, through Babel and Webpack
-import HelloWorld from './components/HelloWorld.vue'
+import Empty from "./components/Empty";
 
 export default {
-  data:function () {
+  name:       "App",
+  data:       function () {
     return {
-      one: "One",
-      two: "Two",
-      message: "testtttt",
-      watchProp: ""
-    };
-  },
-  computed: {
-    // a computed getter
-    printMessage: function () {
-      // `this` points to the vm instance
-      return "hello vue".split('').reverse().join('')
-    },
-    computedWatchProp: function () {
-      return  this.message + " from button but with a computed prop"
-    },
-    secuence: {
-      get: function () {
-        return this.one + "-" + this.two;
-      },
-      set: function (val) {
-        let names = val.split('/')
-        this.one = names[0]
-        this.two = names[1]
-      }
+      descripcion: "",
+      items:       [
+        {
+          id:          1,
+          descripcion: "Some representative placeholder content, with some information about this user. Imagine this being some sort of status update, perhaps?",
+          user:        "@user1"
+        },
+        {
+          id:          2,
+          descripcion: "Some more representative placeholder content, related to this other user. Another status update, perhaps.",
+          user:        "@user2"
+        },
+        {
+          id:          3,
+          descripcion: "This user also gets some representative placeholder content. Maybe they did something interesting, and you really want to highlight this in the recent updates.",
+          user:        "@user3"
+        }
+      ]
     }
   },
-  watch: {
-    message: function() {
-      this.watchProp = this.message + " from button"
-    },
+  methods:    {
+    addItem: function () {
+      this.items.push({
+        id:          this.items.length,
+        descripcion: `${this.descripcion} ${this.items.length}`,
+        user:        "@user1"
+      });
+
+      this.descripcion = "";
+    }
   },
-  me: 'App',
   components: {
-    HelloWorld
+    Empty
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,24 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
-//Vue.config.productionTip = false
+Vue.config.productionTip = false
 
 new Vue({
-  created: function () {
-    // `this` points to the vm instance
-    console.log('created stage')
-  },
-  mounted: function () {
-    // `this` points to the vm instance
-    console.log('mounted stage')
-  },
-  updated: function () {
-    // `this` points to the vm instance
-    console.log('updated stage')
-  },
-  destroyed: function () {
-    // `this` points to the vm instance
-    console.log('destroyed stage')
-  },
-  render: h => h(App),
+    render: h => h(App)
 }).$mount('#app')
