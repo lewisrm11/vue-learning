0ea387c6c23bcded0557a82b0aefbc14c1558f35
Carlos Levi Ricalde May <levi.ricalde@xid.com.mx>
2022-02-26 02:21:59 -0500

    Add vue.config and enable runtime compiler

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+/**
+ * We need this configuration for vue-cli, for void this error in console
+ * [Vue warn]: You are using the runtime-only build of Vue where the template compiler is not available.
+ * Either pre-compile the templates into render functions, or use the compiler-included build.
+ *
+ * @type {{runtimeCompiler: boolean}}
+ */
+module.exports = { runtimeCompiler: true, }
